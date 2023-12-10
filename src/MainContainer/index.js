ddc75dc1a5f26e15935133a146386d976bfdc76f
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-12-10 19:26:43 +0100

    added main container component

---
+++ src/MainContainer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Header';
+
+const MainContainer = () => {
+  return (
+    <div className='py-8 px-24'>
+        <Header />
+    </div>
+  )
+}
+
+export default MainContainer;
